0e3d279646af55ecdc11a875112011793d3646f0
Apurva Saini <apurvasaini@Apurvas-MacBook-Pro-2.local>
2024-09-19 21:38:27 -0400

    added the source code link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
             <li><Link to="/Labs/Lab2">Lab 2</Link></li>
             <li><Link to="/Labs/Lab3">Lab 3</Link></li>
             <li><Link to="/Kanbas">Kanbas</Link></li>
+            <h3><a href="https://github.com/apps-037/kanbas-react-web-app">Source Code Link</a></h3>
         </ul>
     );
 }
