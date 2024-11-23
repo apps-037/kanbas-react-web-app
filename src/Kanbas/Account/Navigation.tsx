fdfb634f98b32d629e0078dff2a458aa2e03d3bf
Apurva Saini <apurvasaini@Apurvas-MacBook-Pro-2.local>
2024-11-22 22:42:56 -0500

    added the server changes

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,43 +1,16 @@
-import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function AccountNavigation() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const { pathname } = useLocation();
-
-  return (
-    <div id="wd-account-navigation" className="wd list-group fs-11 rounded-0">
-      {!currentUser && (
-        <>
-          <Link
-            to={`/Kanbas/Account/Signin`}
-            className={`list-group-item text-danger border border-0 ${
-              pathname === "/Kanbas/Account/Signin" ? "active" : ""
-            }`}
-          >
-            Signin
-          </Link>
-          <Link
-            to={`/Kanbas/Account/Signup`}
-            className={`list-group-item text-danger border border-0 ${
-              pathname === "/Kanbas/Account/Signup" ? "active" : ""
-            }`}
-          >
-            Signup
-          </Link>
-        </>
-      )}
-      {currentUser && (
-        <Link
-          to={`/Kanbas/Account/Profile`}
-          className={`list-group-item text-danger border border-0 ${
-            pathname === "/Kanbas/Account/Profile" ? "active" : ""
-          }`}
-        >
-          Profile
-        </Link>
-      )}
-      <br />
-    </div>
-  );
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+    return (
+        <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+                    <Link to="/Kanbas/Account/Signin" className="list-group-item active border border-0">
+                        Signin
+                    </Link>
+                    <Link to="/Kanbas/Account/Signup" className="list-group-item text-danger border border-0">Signup</Link>
+                    <Link to="/Kanbas/Account/Profile" className="list-group-item text-danger border border-0">Profile</Link>
+        </div>
+    );
 }
